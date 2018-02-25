0a0f75af0d2d2caf3a6f98d4750942c84a6a9b6b
Samwise Rowe <samwise.rowe@gmail.com>
2018-02-25 09:30:14 -0800

    added button logic

--- spelling-site/src/user-dash.component.jsx
+++ spelling-site/src/user-dash.component.jsx
@@ -7,6 +7,26 @@ import {Circle} from 'rc-progress'
 class UserDash extends React.PureComponent {
     constructor(props) {
         super(props);
+        this.state = {
+            percent: 0,
+            userScore: 86
+        }
+
+        this.increase = this.increase.bind(this);
+    }
+
+    componentDidMount() {
+        this.increase();
+    }
+
+    increase() {
+        const percent = this.state.percent + 1;
+        if (percent >= this.state.userScore) {
+            clearTimeout(this.tm);
+            return;
+        }
+        this.setState({ percent });
+        this.tm = setTimeout(this.increase, 10);
     }
 
     render() {
@@ -14,7 +34,7 @@ class UserDash extends React.PureComponent {
             <React.Fragment>
             <header>
                 <Image className="banner" responsive src={banner}/>
-                <Circle className="progress-circle" percent="86" strokeWidth="4" strokeColor="#D3D3D3" />
+                <Circle className="progress-circle" percent={this.state.percent} strokeWidth="6" strokeColor="#5cb85c" />
             </header>
             <section>
 
--- spelling-site/src/word-explorer.component.jsx
+++ spelling-site/src/word-explorer.component.jsx
@@ -4,6 +4,9 @@ import { Button, Glyphicon } from 'react-bootstrap'
 class WordExplorer extends React.PureComponent {
     constructor(props) {
         super(props);
+        this.state = {
+            answer: null,
+        }
         //number of questions in round passed in as prop
     }
 
@@ -12,19 +15,29 @@ class WordExplorer extends React.PureComponent {
         //serviceCall getWordInfo
     }
 
+    onChange(e) {
+        this.setState({ answer: e.target.value });
+    }
+
+    onClick() {
+        //service call to submit answer
+        //.then on successful response give success alert
+        //.catch on error give error alert
+    }
+
     render() {
         return (
             <section>
                 <div className="container align-items-center">
                     <div className="row">
-                        <div style={{ textAlign: 'left' }} className="col-lg-4">
+                        <div style={{ textAlign: 'left', marginBottom: 40 }} className="col-lg-4">
                             <h1>Question 4</h1>
                         </div>
                     </div>
 
                     <div className="page-component">
                         <div className="row">
-                            <div className="col-md-2 col-md-offset-5">
+                            <div className="col-lg-2 col-lg-offset-5 col-md-2 col-md-offset-5 col-sm-2 col-sm-offset-5 col-xs-4 col-xs-offset-4">
                                 <Button type="button" bsStyle="info" bsSize="lg" block>
                                     <Glyphicon glyph="volume-up" />
                                 </Button>
@@ -32,11 +45,11 @@ class WordExplorer extends React.PureComponent {
                         </div>
 
                         <div className="row">
-                            <div className="col-md-6 col-md-offset-3">
+                            <div className="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2">
                                 <div className="form-group">
-                                    <input style={{ marginTop: 20 }} type="text" className="form-control input-lg" placeholder="Missip..." />
+                                    <input style={{ marginTop: 20 }} type="text" className="form-control input-lg" placeholder="Missip..." onChange={this.onChange.bind(this)}/>
                                     <div className="col-md-6 col-md-offset-3">
-                                        <Button style={{ marginTop: 20 }} type="button" bsStyle="success" bsSize="large" block>Spell!</Button>
+                                        <Button style={{ marginTop: 20 }} type="button" onClick ={this.onClick.bind(this)} bsStyle="success" bsSize="large" block>Spell!</Button>
                                     </div>
                                 </div>
                             </div>
