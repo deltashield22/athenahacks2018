726a5b5dc16ebf28c4e623101e0f0af13f33c7a6
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-02-25 03:55:06 -0800

    wip

---
+++ AthenaHacks18/AthenaHacks18.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin.Extensions;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AthenaHacks18.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
