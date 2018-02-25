3fc6e1e4ada0c7185c52152fd19f1d3ccab1fe20
Karre Lawson <karrelawson@gmail.com>
2018-02-24 23:18:06 -0800

    first

---
+++ AthenaHacks18/AthenaHacks18.Services/StudentsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using AthenaHacks18.Domian;
+//using AthenaHacks18.Request;
+//using AthenaHacks18.Domian;
+using AthenaHacks2018.Data.Providers;
+
+
+
+namespace AthenaHacks18.Services
+{
+    public class StudentsService
+    {
+        readonly IDataProvider dataProvider;
+
+    }
+}
