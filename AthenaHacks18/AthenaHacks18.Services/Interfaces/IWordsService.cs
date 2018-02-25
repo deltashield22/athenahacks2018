3706d369d6a3196d41a7e2acba94a7691abed898
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-02-25 01:20:23 -0800

    added WordsController and Unity.WebApi to resolve DependencyInjection issues

---
+++ AthenaHacks18/AthenaHacks18.Services/Interfaces/IWordsService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using AthenaHacks18.Models.Domain;
+using AthenaHacks18.Models.Request;
+
+namespace AthenaHacks18.Services
+{
+    public interface IWordsService
+    {
+        void Create(WordCreateUpdateRequest req);
+        List<Word> GetAll();
+        Word GetByGrade(int grade);
+        Word GetByWord(string wordStr);
+        void Update(WordCreateUpdateRequest req);
+    }
+}
---
+++ AthenaHacks18/AthenaHacks18.Web/Controllers/WordsController.cs
@@ -0,0 +1,93 @@
+﻿using AthenaHacks18.Models.Domain;
+using AthenaHacks18.Models.Request;
+using AthenaHacks18.Services;
+using AthenaHacks18.Models.Responses;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AthenaHacks18.Web.Controllers
+{
+    [AllowAnonymous]
+    public class WordsController : ApiController
+    {
+        readonly IWordsService wordsService;
+
+        public WordsController(IWordsService wordsService)
+        {
+            this.wordsService = wordsService;
+        }
+
+        [HttpGet, Route("api/words")]
+        public HttpResponseMessage GetAll()
+        {
+            List<Word> words = wordsService.GetAll();
+
+            ItemsResponse<Word> response = new ItemsResponse<Word>();
+            response.Items = words;
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+        [HttpGet, Route("api/words/{word}")]
+        public HttpResponseMessage GetByWord(string wordStr)
+        {
+            Word word = wordsService.GetByWord(wordStr);
+
+            ItemResponse<Word> response = new ItemResponse<Word>();
+            response.Item = word;
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+
+        [HttpGet, Route("api/words/{grade:int}")]
+        public HttpResponseMessage GetByGrade(int grade)
+        {
+            Word word = wordsService.GetByGrade(grade);
+
+            ItemResponse<Word> response = new ItemResponse<Word>();
+            response.Item = word;
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+        [HttpPost, Route("api/words")]
+        public HttpResponseMessage Create(WordCreateUpdateRequest req)
+        {
+            if (req == null)
+            {
+                ModelState.AddModelError("", "No model was sent.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            wordsService.Create(req);
+
+
+            return Request.CreateResponse(HttpStatusCode.Created, new SuccessResponse());
+        }
+
+        [HttpPut, Route("api/words")]
+        public HttpResponseMessage Update(WordCreateUpdateRequest req)
+        {
+            if (req == null)
+            {
+                ModelState.AddModelError("", "No model was sent.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            wordsService.Update(req);
+
+            return Request.CreateResponse(HttpStatusCode.OK, new SuccessResponse());
+        }
+    }
+}
