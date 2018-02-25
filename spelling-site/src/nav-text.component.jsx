8e80ddb1bc771a79f360d06f1aa51a5323e0f49d
Samwise Rowe <samwise.rowe@gmail.com>
2018-02-24 17:57:12 -0800

    started initial page

---
+++ spelling-site/src/nav-text.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+// import userService from '???'
+
+class NavText extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+        // userService.getLoggedInUser() 
+        //     .then(user => {
+        //         this.setState({user: user});
+        //     })
+        //     .catch(err => {
+        //         console.log("Whoops! " + err);
+        //     })
+    }
+
+    render() {
+        // let navText = user.id ? <React.Fragment>Welcome back, {user.name}</React.Fragment> : Login;
+        // return(
+        //     {navText}
+        // );
+    }
+}
